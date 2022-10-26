1fc1496cdbd4411577b8a8e31b7a7165160db200
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2022-10-26 15:17:33 -0400

    Create Disable_UnauthorizedUsers.ps1

---
+++ Disable_UnauthorizedUsers.ps1
@@ -0,0 +1,20 @@
+# todo turn into a proper script
+
+
+function Disable-UnauthorizedUsers {
+    param (
+        $users
+    )
+
+    foreach ($user in Get-LocalUser) {
+        #Write-Host $users.Contains($user.Name)
+        if((!$users.Contains($user.Name)) -and $user.Enabled){
+            Write-Host "Bad user found!"
+            Write-Host $user.Name
+            if((Read-Host "Do you want to disable this user? (y/n)") -eq "y"){
+                Write-Host "Disabling user..."
+                Disable-LocalUser $user.SID
+            } 
+        }
+    }
+}
