73658e03538dd481a4ab789865bb5d21bd09a438
Catalyst4 <catalyst4222@gmail.com>
2022-10-31 15:21:56 +0000

    Let parse_users.py default to users

--- parse_users.py
+++ parse_users.py
@@ -4,8 +4,8 @@ import argparse
 
 def parse_readme(full_text: str) -> tuple[list[str], list[str]]:
 
-	admin_block = full.split("\n\n")[0].split("\n")[1:]
-	user_block = full.split("\n\n")[1].split("\n")[1:]
+	admin_block = full_text.split("\n\n")[0].split("\n")[1:]
+	user_block = full_text.split("\n\n")[1].split("\n")[1:]
 
 	admin_block[0] = admin_block[0][:-6]
 
@@ -22,13 +22,13 @@ def parse_readme(full_text: str) -> tuple[list[str], list[str]]:
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Process the list of users from the README')
 	parser.add_argument("text", help="The block of users from the README. Include the headers for the sections, and the passwords")
-	parser.add_argument("target", choices=["users", "passwords", "both"])
+	parser.add_argument("target", choices=["users", "passwords", "both"], nargs="?", const="users", type=str)
 
 	args = parser.parse_args()
 		
 	users, passwords = parse_readme(args.text)
 
-	if args.target == "users":
+	if args.target == "users" or args.target is None:
 		print('\n'.join(users))
 	elif args.target == "passwords":
 		print("\n".join(passwords))
