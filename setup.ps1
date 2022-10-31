8894e75aa1fc5ae493318f64c7b346c58a9a35a1
Catalyst4 <catalyst4222@gmail.com>
2022-10-31 15:27:18 +0000

    add a setup script to download everything needed

---
+++ setup.ps1
@@ -0,0 +1,13 @@
+﻿# download chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# download stuff with chocolatey
+choco feature enable -n allowGlobalConfirmation
+choco install git -y
+
+# refresh environment
+refreshenv
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+
