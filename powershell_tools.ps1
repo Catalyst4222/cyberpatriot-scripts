8299279aeb8cc1496397406ddaacff378fb49393
Catalyst4 <catalyst4222@gmail.com>
2022-12-21 11:12:51 -0500

    powershell tools

---
+++ powershell_tools.ps1
@@ -0,0 +1,12 @@
+
+# Get all Non-default services
+$NonDefaultServices = Get-wmiobject win32_service | where { $_.Caption -notmatch "Windows" -and $_.PathName -notmatch "Windows" -and $_.PathName -notmatch "policyhost.exe" -and $_.Name -ne "LSM" -and $_.PathName -notmatch "OSE.EXE" -and $_.PathName -notmatch "OSPPSVC.EXE" -and $_.PathName -notmatch "Microsoft Security Client" }
+
+# $NonDefaultServices.DisplayName # Service Display Name (full name)
+# $NonDefaultServices.PathName # Service Executable
+# $NonDefaultServices.StartMode # Service Startup mode
+# $NonDefaultServices.StartName # Service RunAs Account
+# $NonDefaultServices.State # Service State (running/stopped etc)
+# $NonDefaultServices.Status # Service Status
+# $NonDefaultServices.Started # Service Started status
+# $NonDefaultServices.Description # Service Description
